60e080c5657e8ea7e503756dd33d6b9da8d72236
forsaken <alexey2142@mail.ru>
2017-10-09 14:50:47 +1000

    add ordering

--- src/controllers/home_controller.cr
+++ src/controllers/home_controller.cr
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    races = Race.ordered
+    races = Race.search_by_sql "SELECT * FROM races ORDER BY TO_TIMESTAMP(races.date, 'DD.MM.YYYY') DESC"
     render("index.slang")
   end
 
