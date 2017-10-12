a197f94eb38dcd61d542e3cdb24931a5ec3fc15d
forsaken <alexey2142@mail.ru>
2017-10-12 10:51:03 +1000

    order icons

--- src/controllers/races_controller.cr
+++ src/controllers/races_controller.cr
@@ -4,10 +4,21 @@ class RacesController < ApplicationController
     render("show.slang")
   end
 
+  # Current race
   private def race
     @race ||= Race.find!(params["id"]).as(Race)
   end
 
+  # Sort icon html helper
+  private def sort_icon(position)
+    if (position.nil? && !params["position"]?) || (params["position"]? && position == params["position"].to_i)
+      "<i class=\"fa fa-sort-#{order}\"></i>"
+    else
+      "<i class=\"fa fa-sort\"></i>"
+    end
+  end
+
+  # Safe current order
   private def order
     if params["order"]? && params["order"] == "desc"
       "desc"
@@ -16,6 +27,7 @@ class RacesController < ApplicationController
     end
   end
 
+  # Safe SQL for sorting by groups
   private def group_id_sql
     if params["group_id"]?
       "AND results.group_id = #{params["group_id"].to_i}"
@@ -24,6 +36,7 @@ class RacesController < ApplicationController
     end
   end
 
+  # Filter results for current race
   private def results
     @results ||= if params["position"]?
       Result.search_by_sql(
@@ -40,16 +53,18 @@ class RacesController < ApplicationController
         "SELECT results.* FROM results
         WHERE results.race_id = $1
         #{group_id_sql}
-        ORDER BY TO_TIMESTAMP(results.time, 'HH.MI.SS') #{order}",
+        ORDER BY TO_TIMESTAMP(results.time, 'HH24.MI.SS') #{order}",
         [race.id]
       )
     end.as(Array(Result))#.includes(:result_race_disciplines).includes(:user).as(Jennifer::QueryBuilder::ModelQuery(Result))
   end
 
+  # All results for current race
   private def all_results
     @all_results ||= Result.where { _race_id == race.id }.as(Jennifer::QueryBuilder::ModelQuery(Result))
   end
 
+  # Groups list from all results for race
   private def groups
     @groups ||= all_results.to_a.map do |result|
       result.group
@@ -58,10 +73,12 @@ class RacesController < ApplicationController
     end.as(Array(Group))
   end
 
+  # For tabs
   private def active?(group)
     params["group_id"]? && params["group_id"].to_i == group.id
   end
 
+  # Route helper for race
   private def race_path(race, **attributes)
     joined_attrs = unless attributes.empty?
               "?" + attributes.map do |key, value|
