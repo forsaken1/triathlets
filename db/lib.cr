b1b37f777034446c0bb0b945b37f9743141ba989
Krylov Alexey <alexey2142@mail.ru>
2018-07-03 17:02:05 +1000

    add lib for seeds

---
+++ db/lib.cr
@@ -0,0 +1,25 @@
+def add_result(race, group_name, user_name, city_name, team_name, *results)
+  add_result(race, group_name, user_name, city_name, team_name, results)
+end
+
+def add_result(race, group_name, user_name, city_name, team_name, results = [] of String)
+  results_array = results.to_a
+  all_result = results_array.shift
+  result_attrs = {user_id: USERS[user_name], race_id: race.id, city_id: CITIES[city_name], team_id: TEAMS[team_name], time: all_result}.to_h
+  result_attrs.merge!({group_id: GROUPS[group_name]}.to_h) if group_name
+  result = Result.create result_attrs
+
+  results_array.each_with_index do |res, i|
+    ResultRaceDiscipline.create(result_id: result.id, race_discipline_id: race.race_disciplines[i].id, position: i, time: res)
+  end
+end
+
+def relative_to_absolute(*results)
+  [results.last, results.first] + results.to_a.rotate.map_with_index do |e, i|
+    if e != nil && results[i] != nil
+      (Time.parse(e.as(String), "%X") - Time.parse(results[i].as(String), "%X")).to_s
+    else
+      nil
+    end
+  end[0...results.size - 1]
+end
