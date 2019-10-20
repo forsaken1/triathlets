4b2e27ca0faa549bd11d3f2da5216872ab238340
forsaken1 <alexey2142@mail.ru>
2019-10-21 04:25:00 +1000

    init compare functionality

--- src/controllers/home_controller.cr
+++ src/controllers/home_controller.cr
@@ -4,7 +4,23 @@ class HomeController < ApplicationController
     render("index.slang")
   end
 
+  def compare
+    # users = User.all.where { _id.in(user_ids) } # TODO: failed as Crystal compiler error
+    users = take_users
+    render("compare.slang")
+  end
+
   def about
     render("about.slang")
+  end
+
+  private def take_users
+    @params["user_ids"]? ?
+      User.all.find_by_sql("SELECT * FROM users WHERE id IN (#{user_ids})") :
+      [] of User
+  end
+
+  private def user_ids
+    @params["user_ids"].split(',').uniq.join(",")
   end
 end
