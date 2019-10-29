4f15ef7ebb86db857cdc818a829a2700a755e95e
forsaken1 <alexey2142@mail.ru>
2019-10-29 17:42:56 +1000

    update dependencies and complete users results with orrdering

--- src/helpers/races_presenter.cr
+++ src/helpers/races_presenter.cr
@@ -2,12 +2,12 @@ class RacesPresenter
   @results : Array(Result)
 
   def initialize(@races : Array(Race), @user : User)
-    @results = Result.all.where { _user_id == @user.id & _race_id.in(@races.map(&.id)) }.to_a
+    @results = Result.all.where { and(_user_id == @user.id, _race_id.in(@races.map(&.id))) }.to_a
   end
 
   def each_with_index(&callback : RacePresenter, Int32 -> String::Builder)
     @races.each_with_index do |race, i|
-      yield RacePresenter.new(race, nil), i
+      yield RacePresenter.new(race, time_by_race(race)), i
     end
   end
 
