3388345ad2a638b3f32d4f0cb63c3c70d2ba4242
forsaken1 <alexey2142@mail.ru>
2019-10-30 06:43:46 +1000

    presenters improvements

--- src/controllers/users_controller.cr
+++ src/controllers/users_controller.cr
@@ -6,17 +6,11 @@ class UsersController < ApplicationController
 
   def show
     user = User.find! params["id"]
-    races = Race.by_user_id(user.id, order_query).to_a
     user_presenter = UserPresenter.new user
-    races_presenter = RacesPresenter.new races, user
     render("show.slang")
   end
 
   private def name
     params["name"]? ? params["name"] : ""
-  end
-
-  private def order_query
-    "ORDER BY TO_TIMESTAMP(races.date, 'DD.MM.YYYY')"
   end
 end
