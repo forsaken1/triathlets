166362cb74b8dd1efd540f2986c9e358a0ed7695
forsaken1 <alexey2142@mail.ru>
2019-10-29 18:52:55 +1000

    add position

--- src/helpers/race_presenter.cr
+++ src/helpers/race_presenter.cr
@@ -1,7 +1,7 @@
 class RacePresenter
-  getter :time
+  getter :time, :position
 
-  def initialize(@race : Race, @time : String | Nil)
+  def initialize(@race : Race, @time : String | Nil, @position : Int32 | Nil)
   end
 
   delegate :id, :title, :date, to: @race
