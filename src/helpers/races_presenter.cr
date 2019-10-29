ade1f9b5db4060d65d8de962207cd052408ac1aa
forsaken1 <alexey2142@mail.ru>
2019-10-29 18:52:55 +1000

    add position

--- src/helpers/races_presenter.cr
+++ src/helpers/races_presenter.cr
@@ -7,11 +7,19 @@ class RacesPresenter
 
   def each_with_index(&callback : RacePresenter, Int32 -> String::Builder)
     @races.each_with_index do |race, i|
-      yield RacePresenter.new(race, time_by_race(race)), i
+      yield RacePresenter.new(race, time_by_race(race), position_by_race(race)), i
     end
   end
 
+  private def result(race : Race)
+    @results.find { |result| result.race_id == race.id }.as(Result)
+  end
+
   private def time_by_race(race : Race)
-    @results.find { |result| result.race_id == race.id }.as(Result).time_format
+    result(race).time_format
+  end
+
+  private def position_by_race(race : Race)
+    result(race).position
   end
 end
