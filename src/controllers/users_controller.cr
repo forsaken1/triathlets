f684bac683e39a427beccf3f0ec51769b7bdcbea
forsaken1 <alexey2142@mail.ru>
2019-11-01 06:25:08 +1000

    another presenters implementation

--- src/controllers/users_controller.cr
+++ src/controllers/users_controller.cr
@@ -1,17 +1,29 @@
 class UsersController < ApplicationController
+  ORDER_QUERY = "ORDER BY TO_TIMESTAMP(races.date, 'DD.MM.YYYY')"
+
   def index
-    _users = User.search(name).to_a
-    users = UserPresenter.wrap _users
+    users = User.search(name).to_a
+    users_presenter = UsersPresenter.new users
     render("index.slang")
   end
 
   def show
-    user = User.find! params["id"]
+    user = get_user
+    races = get_races(user)
     user_presenter = UserPresenter.new user
+    races_presenter = RacesPresenter.new races, user
     render("show.slang")
   end
 
   private def name
     params["name"]? ? params["name"] : ""
+  end
+
+  private def get_user
+    User.find! params["id"]
+  end
+
+  private def get_races(user)
+    Race.by_user_id(user.id, ORDER_QUERY).to_a
   end
 end
